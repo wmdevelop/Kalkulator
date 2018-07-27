e4f6a4cd9a485b50dabfea8843521b41b73b047a
wmdevelop <wmdev07@gmail.com>
2018-07-27 15:46:50 +0200

    Adding substraction  function

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 double add(double&a,double&b){return a+b;}
 double add(double a,double b){return a+b;}
 
+double substr(double&a,double&b){return a-b;}
+double substr(double a,double b){return a-b;}
+
 
 int main(){
 	cout<<add(2.11,3.44)<<endl;
+	cout<<substr(4.11,3.44)<<endl;
 }
